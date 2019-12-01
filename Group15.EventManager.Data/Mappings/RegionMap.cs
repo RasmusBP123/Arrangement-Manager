161eda8e8222ba74c94bddbcfce7c98e1a434960
Rasmus Bak Petersen <rasm105k@edu.eal.dk>
2019-12-01 11:39:38 +0100

    Region Seed

--- Group15.EventManager.Data/Mappings/RegionMap.cs
+++ Group15.EventManager.Data/Mappings/RegionMap.cs
@@ -18,8 +18,21 @@ namespace Group15.EventManager.Data.Mappings
             builder.HasData(
                 new Region
                 {
-                    Id = Guid.NewGuid()
-                }); ;
+                    Id = new Guid("0d096733-5437-4c53-8f3d-0e815d3753b3"),
+                    Name = "Midtjylland",
+                });
+            builder.HasData(
+                new Region
+                {
+                    Id = new Guid("4c68ec57-cfed-4b52-be86-6349d47f9a74"),
+                    Name = "Nordjylland",
+                });
+            builder.HasData(
+                new Region
+                {
+                    Id = new Guid("2469efa8-72a6-490f-bf5a-b434e672bc78"),
+                    Name = "Sjælland",
+                });
         }
     }
 }
